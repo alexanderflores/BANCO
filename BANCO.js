31e7c0299e4e830f9a4dd6238506da2f3585663c
alexander <alexbrandodoo@gamil.com>
2016-09-26 19:00:19 -0400

    termine de hacer el primer metodo de dar turno, faltan los dos metodos sigientes para terminar el requerimiento

--- BANCO.js
+++ BANCO.js
@@ -1,3 +1,24 @@
+
+var contador=0;
 function accionradio(tipodefuncion){
-	alert('voy bien'+tipodefuncion)
+	switch (tipodefuncion){
+		case 'tn':
+		darnumero();
+		break;
+		case 'dp':
+		alert('ejemplo de tipo de cuenta')
+		break;
+		default:
+		alert('ejemplo de nuemro de cuenta')
+		break;
+		}
+	//alert('su numero es 1'+tipodefuncion)
+	}
+
+function darnumero(){
+	
+			 var cedula = prompt("numero de cedula");
+			 var resultados= 'su turno es '+cedula+'-'+contador;
+			 contador=contador+1;
+			 alert(resultados)
 	}
